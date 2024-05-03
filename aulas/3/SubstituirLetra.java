adf574e7280b408279f8cdb86b9ee1a33dea9c61
awangelo <anggg.gaio@gmail.com>
2024-05-03 17:21:02 -0300

    replaceAll usado para substituir vogais

--- aulas/3/SubstituirLetra.java
+++ aulas/3/SubstituirLetra.java
@@ -7,19 +7,9 @@ public class SubstituirLetra {
 		System.out.print("Insira uma frase: ");
 		String frase = sc.nextLine();
 		sc.close();
+
+		String fraseTrocada = frase.replaceAll("[aeiouAEIOU]", "");
 		
-		String fraseTrocada = frase.replace('a', '');
-		String fraseTrocada = frase.replace('e', '');
-		String fraseTrocada = frase.replace('i', '');
-		String fraseTrocada = frase.replace('o', '');
-		String fraseTrocada = frase.replace('u', '');
-		
-		String[] array = fraseTrocada.split("");
-		
-		for (int i = 0; i < array.length(); i++) {
-			
-		}
-		
-		System.out.println("Frase com letras trocadas: " + fraseTrocada);
+		System.out.println("String sem vogais: " + fraseTrocada);
 	}
 }
