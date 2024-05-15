43f9a8e1243fb3a0854d60dd2d40c6aa1edc50af
angelo <99550408+awangelo@users.noreply.github.com>
2024-05-15 17:02:47 +0000

    fix

--- aulas/6/Atv1.java
+++ aulas/6/Atv1.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Atv1 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int ele[] = new int[7];
-		
-		System.out.print("Insira o numero a ser adcionado: ");
-		int n = sc.nextInt();
-		
-		ele[0] = n;
-		ele[6] = n;
-		
-		for (int i = 0; i < n; i++) {
-			System.out.println(ele[i]);
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int ele[] = new int[7];
+
+        System.out.print("Insira o numero a ser adicionado: ");
+        int n = sc.nextInt();
+
+        ele[0] += n;
+        ele[6] += n;
+
+        for (int i = 0; i < ele.length; i++) {
+            System.out.println(ele[i]);
+        }
+
+        sc.close();
+    }
 }
