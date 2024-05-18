94d8c87e5ac58f1d2d9ba80080a439c3a4ac93e4
angelo <99550408+awangelo@users.noreply.github.com>
2024-05-18 19:41:23 +0000

    Update App.java

--- OOP/Estoque/App.java
+++ OOP/Estoque/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
+import entity.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        Estoque estoque = new Estoque(100, 100); // max 100 produtos e 100 transacoes (tamanho do array)
+        Scanner scanner = new Scanner(System.in);
     }
 }
